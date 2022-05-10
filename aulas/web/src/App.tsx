442a7d6f6e6f892f9326005510850c89511cfb8e
Jose Pena <>
2022-05-09 23:28:53 -0300

    Using className

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,7 +1,20 @@
 import { useState } from 'react'
 
+interface ButtonProps{
+  text?: string;
+}
+
+function Button(props: ButtonProps){
+  return <button className="bg-violet-500 p-2 rounded">{props.text ?? 'Default'}</button>
+}
+
 function App() {
-  return <h1>Hello World</h1>
+  return(
+    <div className="flex gap-2">
+      <Button text='Enviar'/>
+      <Button text='Ok'/>
+    </div>
+  )
 }
 
 export default App
