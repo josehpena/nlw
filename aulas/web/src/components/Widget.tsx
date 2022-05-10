4b9e9854eda76f94d0bf5169a69fbf9e4460c918
Jose Pena <>
2022-05-10 00:42:28 -0300

    Accessibility has been added

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { useState } from 'react';
+import { Popover } from '@headlessui/react';
 
 export function Widget(){
     const [isWidgetOpen, setIsWidgetOpen] = useState(false);
@@ -10,11 +11,10 @@ export function Widget(){
 
 
     return (
-        <div className='absolute bottom-4 right-4'>
-            {isWidgetOpen && <p>Hello World</p> }
+        <Popover className='absolute bottom-4 right-4'>
+            <Popover.Panel><p>Hello World</p></Popover.Panel>
 
-
-             <button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+             <Popover.Button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6' />
 
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
@@ -22,7 +22,8 @@ export function Widget(){
                         Feedback
                     </span>
                 </span>
-            </button>
-        </div>
+                
+            </Popover.Button>
+        </Popover>
     )
 }
